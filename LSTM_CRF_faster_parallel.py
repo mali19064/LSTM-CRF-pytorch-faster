f37947c548395d1a5914b4df216e6e54e25922b7
MA LI <55191719+mali19064@users.noreply.github.com>
2020-06-16 14:05:21 +0800

    Update LSTM_CRF_faster_parallel.py

--- LSTM_CRF_faster_parallel.py
+++ LSTM_CRF_faster_parallel.py
@@ -5,12 +5,8 @@ import torch.optim as optim
 
 START_TAG = "<START>"
 STOP_TAG = "<STOP>"
-EMBEDDING_DIM = 5
-HIDDEN_DIM = 4
-
 
 # torch.manual_seed(1)
-
 
 def argmax(vec):
     # return the argmax as a python int
